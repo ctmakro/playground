7ff2c54906e9321157a25e3c3f29919f39919a55
ctmakro <ctmakro@gmail.com>
2017-08-23 21:59:58 +0800

    better trilaterate code

--- gcode/delta.py
+++ gcode/delta.py
@@ -60,45 +60,47 @@ class delta:
             slider_coords_x[i], slider_coords_y[i], slider_coords_z[i]
         ]) for i in [0,1,2]]
 
-        # print('ash',actual_sliders_height)
-        # for s in sliders:
-        #     print('slider cartesian', s)
-        def normalized(v):
-            norm = np.sqrt(np.sum(v**2))
-            return v / norm
-        # Trilateration to find the target point, given slider positions
-        def trilaterate(sliders):
+        # def normalized(v):
+        #     norm = np.sqrt(np.sum(v**2))
+        #     return v / norm
+        # # Trilateration to find the target point, given slider positions
+        # def trilaterate(sliders):
+        #
+        #     # initial guess
+        #     t = np.array([0,0,-10]).astype('float32')
+        #     v = t*0
+        #     for i in range(1000):
+        #         # print('t@',i,t)
+        #         total_force = v*0
+        #         tick = 0
+        #         for slider in sliders:
+        #             diff = slider - t # direction vector towards sphere center
+        #             delta_dist = (np.sqrt(np.sum(diff**2)) - self.drl) # distance difference
+        #
+        #             force = delta_dist * normalized(diff)
+        #             total_force+=force
+        #             # print('dtdist',delta_dist)
+        #
+        #             if abs(delta_dist) < 1e-2:
+        #                 tick+=1
+        #
+        #         if tick>=3:
+        #             break
+        #         else:
+        #             tick=0
+        #
+        #         v += total_force # dampen-spring simulation
+        #         v *= 0.9
+        #         t += v
+        #     print('(fk) took',i+1,'iteration to solve trilateration')
+        #     return t
 
-            # initial guess
-            t = np.array([0,0,-10]).astype('float32')
-            v = t*0
-            for i in range(1000):
-                # print('t@',i,t)
-                total_force = v*0
-                tick = 0
-                for slider in sliders:
-                    diff = slider - t # direction vector towards sphere center
-                    delta_dist = (np.sqrt(np.sum(diff**2)) - self.drl) # distance difference
+        def trilaterate2(sliders):
+            from trilaterate import trilaterate as trl
+            a,b = trl(sliders, np.array([self.drl]*3))
+            return a if a[2]<b[2] else b
 
-                    force = delta_dist * normalized(diff)
-                    total_force+=force
-                    # print('dtdist',delta_dist)
-
-                    if abs(delta_dist) < 1e-2:
-                        tick+=1
-
-                if tick>=3:
-                    break
-                else:
-                    tick=0
-
-                v += total_force # dampen-spring simulation
-                v *= 0.9
-                t += v
-            print('(fk) took',i+1,'iteration to solve trilateration')
-            return t
-
-        t = trilaterate(sliders)
+        t = trilaterate2(sliders)
         return t
 
     # inverse kinematics
@@ -142,3 +144,78 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+def ttrilaterate(points, distances, return_middle):
+    import numpy as np
+    p1,p2,p3 = points
+    # point -> np arrays in the form of [x, y, z]
+    # distances -> np array [r1, r2, r3]
+    def norm(v):
+        return np.sqrt(np.sum(v**2))
+    def dot(v1,v2):
+        return np.dot(v1,v2)
+    def cross(v1,v2):
+        return np.cross(v1,v2)
+
+    ex = (p2-p1) /  norm(p2-p1)
+    i = dot(ex,p3-p1)
+    a = (p3-p1) - ex*i
+    ey = a/norm(a)
+    ez = cross(ex,ey)
+    d = norm(p2-p1)
+    j = dot(ey,p3-p1)
+    x = (distances[0]**2 - distances[1]**2 + d**2) / (2*d)
+    y = (distances[0]**2 - distances[2]**2 + i**2 + j**2) / (2*j) - (i/j) * x
+    b = distances[0]**2 - x**2 - y**2
+
+    z = np.sqrt(b)
+    if math.isnan(z):
+        raise Exception('NaN met, cannot solve for z')
+
+    a = p1 + ex*x + ey*y
+    p4a = a + ez*z
+    p4b = a - ez*z
+
+    return p4a, p4b
+
+# 	i = dot(ex, vector_subtract(p3, p1));
+# 	a = vector_subtract(vector_subtract(p3, p1), vector_multiply(ex, i));
+# 	ey = vector_divide(a, norm(a));
+# 	ez =  vector_cross(ex, ey);
+# 	d = norm(vector_subtract(p2, p1));
+# 	j = dot(ey, vector_subtract(p3, p1));
+#
+#
+# 	x = (sqr(p1.r) - sqr(p2.r) + sqr(d)) / (2 * d);
+# 	y = (sqr(p1.r) - sqr(p3.r) + sqr(i) + sqr(j)) / (2 * j) - (i / j) * x;
+#
+# 	b = sqr(p1.r) - sqr(x) - sqr(y);
+#
+# 	// floating point math flaw in IEEE 754 standard
+# 	// see https://github.com/gheja/trilateration.js/issues/2
+# 	if (Math.abs(b) < 0.0000000001)
+# 	{
+# 		b = 0;
+# 	}
+#
+# 	z = Math.sqrt(b);
+#
+# 	// no solution found
+# 	if (isNaN(z))
+# 	{
+# 		return null;
+# 	}
+#
+# 	a = vector_add(p1, vector_add(vector_multiply(ex, x), vector_multiply(ey, y)))
+# 	p4a = vector_add(a, vector_multiply(ez, z));
+# 	p4b = vector_subtract(a, vector_multiply(ez, z));
+#
+# 	if (z == 0 || return_middle)
+# 	{
+# 		return a;
+# 	}
+# 	else
+# 	{
+# 		return [ p4a, p4b ];
+# 	}
+# }
---
+++ gcode/trilaterate.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def trilaterate(points, distances):
+    # ported from https://github.com/gheja/trilateration.js
+    # points -> list of np arrays in the form of [[x, y, z], [x, y, z]
+    # distances -> np array [r1, r2, r3]
+    p1,p2,p3 = points
+    r1,r2,r3 = distances
+    def norm(v):
+        return np.sqrt(np.sum(v**2))
+    def dot(v1,v2):
+        return np.dot(v1,v2)
+    def cross(v1,v2):
+        return np.cross(v1,v2)
+
+    ex = (p2-p1) / norm(p2-p1)
+    i = dot(ex, p3-p1)
+    a = (p3-p1) - ex*i
+    ey = a / norm(a)
+    ez = cross(ex, ey)
+    d = norm(p2-p1)
+    j = dot(ey, p3-p1)
+    x = (r1**2 - r2**2 + d**2) / (2*d)
+    y = (r1**2 - r3**2 + i**2 + j**2) / (2*j) - (i/j) * x
+    b = r1**2 - x**2 - y**2
+
+	# floating point math flaw in IEEE 754 standard
+	# see https://github.com/gheja/trilateration.js/issues/2
+    if (np.abs(b) < 0.0000000001):
+        b = 0
+
+    z = np.sqrt(b)
+    if np.isnan(z):
+        raise Exception('NaN met, cannot solve for z')
+
+    a = p1 + ex*x + ey*y
+    p4a = a + ez*z
+    p4b = a - ez*z
+
+    return p4a, p4b
